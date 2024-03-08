bacf1634158c70635d758df621abe46979d3c82b
KandepiBhavani23 <kandepibhavani@gmail.com>
2024-03-08 23:21:15 +0530

    Power2.js

---
+++ JS2024-Problem-Solving/2024-March/JS18-Power2.js
@@ -0,0 +1,18 @@
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+*/
+
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(2 ** i);
+  }
+  return result;
+}
+
+console.log(powersOfTwo(0));
+console.log(powersOfTwo(2));
+console.log(powersOfTwo(5));
+console.log(powersOfTwo(9));
+console.log(powersOfTwo(10));
+console.log(powersOfTwo(3));
